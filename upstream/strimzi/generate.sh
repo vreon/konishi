c8702dda844cb7c0542c6bd06cfa59dd0f6802cc
Jesse Dubay <jesse@jessedubay.com>
2019-07-26 22:53:40 -0700

    Upgrade to Strimzi 0.13.0-rc1

--- upstream/strimzi/generate.sh
+++ upstream/strimzi/generate.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-curl -Ls https://github.com/strimzi/strimzi-kafka-operator/releases/download/0.12.1/strimzi-cluster-operator-0.12.1.yaml \
+curl -Ls https://github.com/strimzi/strimzi-kafka-operator/releases/download/0.13.0-rc1/strimzi-cluster-operator-0.13.0-rc1.yaml \
     > strimzi-cluster-operator.yaml
 
 touch kustomization.yaml
