ee808a7276423cc2e1cb8cebd54891e66de7e8f0
Jesse Dubay <jesse@jessedubay.com>
2019-10-13 14:41:45 -0700

    Update external-dns to 0.5.17

--- upstream/external-dns/generate.sh
+++ upstream/external-dns/generate.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-curl -s https://raw.githubusercontent.com/kubernetes-incubator/external-dns/v0.5.15/docs/tutorials/aws.md | \
+curl -s https://raw.githubusercontent.com/kubernetes-incubator/external-dns/v0.5.17/docs/tutorials/aws.md | \
     sed -n '101,161p' \
     > external-dns.yaml
 
 touch kustomization.yaml
 kustomize edit add resource external-dns.yaml
-kustomize edit set image registry.opensource.zalan.do/teapot/external-dns:v0.5.15
+kustomize edit set image registry.opensource.zalan.do/teapot/external-dns:v0.5.17
