3c3e121aa1e041b5a9237fb12ef531d3cbfc295b
Jesse Dubay <jesse@jessedubay.com>
2019-10-18 20:40:14 -0700

    Update kubeless to 1.0.5

--- upstream/kubeless/generate.sh
+++ upstream/kubeless/generate.sh
@@ -2,9 +2,7 @@
 
 set -e
 
-curl -sL https://github.com/kubeless/kubeless/releases/download/v1.0.4/kubeless-v1.0.4.yaml | \
-    sed 's/apps\/v1beta1/apps\/v1/g' | \
-    sed 's/kubeless\/function-controller:v1.0.4/kubeless\/function-controller:build-30de1835-37dc-40be-92c3-d3d73464d975/g' \
+curl -sL https://github.com/kubeless/kubeless/releases/download/v1.0.5/kubeless-v1.0.5.yaml \
     > kubeless.yaml
 
 touch kustomization.yaml
