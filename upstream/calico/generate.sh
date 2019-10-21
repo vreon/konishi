5cdc0d581ecf57d88ec21f6349de78d08f99ac16
Jesse Dubay <jesse@jessedubay.com>
2019-10-20 19:18:34 -0700

    Update Calico to 3.10.0

--- upstream/calico/generate.sh
+++ upstream/calico/generate.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-curl -s https://docs.projectcalico.org/v3.8/manifests/calico.yaml > calico.yaml
+curl -s https://docs.projectcalico.org/v3.10/manifests/calico.yaml > calico.yaml
 
 touch kustomization.yaml
 kustomize edit add resource calico.yaml
