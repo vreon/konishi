ebf15240bee1cac003dc4817330f3ab56a68af2e
Jesse Dubay <jesse@jessedubay.com>
2019-10-20 19:44:20 -0700

    Update Loki and Promtail to 0.3.0

--- upstream/loki/generate.sh
+++ upstream/loki/generate.sh
@@ -5,11 +5,11 @@ set -e
 helm repo add loki https://grafana.github.io/loki/charts
 helm repo update
 
-helm fetch loki/loki --version 0.13.0 --untar
+helm fetch loki/loki --version 0.16.0 --untar
 helm template --namespace loki -n loki loki > loki.yaml
 rm -rf loki
 
-helm fetch loki/promtail --version 0.11.0 --untar
+helm fetch loki/promtail --version 0.12.3 --untar
 helm template --namespace loki -n promtail \
     --set loki.serviceName=loki \
     promtail > promtail.yaml
